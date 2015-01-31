7a935e5710f9445527faa663b08158133c0a210d
biroa <adam.biro@gmail.com>
2015-01-31 20:31:33 +0000

    more+3

--- func.js
+++ func.js
@@ -1,9 +1,16 @@
 (function(){
-function player(name){
+function player(name,pos){
     this.name = name;
+    this.position = pos;
 }
 
 console.log("=====================");
-var constructor = new player('Tibi');
+
+var constructor = new player('Tibi','striker');
 console.log(constructor.name);
+console.log(constructor.position);
+var constructor2 = new player('Laci','defender');
+console.log(constructor2.name);
+console.log(constructor2.position);
+
 })();
