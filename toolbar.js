c7cc4775629a4159ce2449f08a7838468b4e03a4
biroa <adam.biro@gmail.com>
2015-02-11 21:23:07 +0000

    toolbar access comment

--- toolbar.js
+++ toolbar.js
@@ -1,2 +1,14 @@
-var toolbar = oojs.createToolbar("firstToolbar");
-toolbar.items[0];
+/*
+    Access of toolbar
+
+    var toolbar = oojs.createToolbar("firstToolbar");
+    var toolbarItem = toolbar.items[0];
+
+    toolbarItem.getEnabled(true); // or fase
+    toolbarItem.getEnabled();
+
+    toolbarItem.enabled = true; // or fasle
+
+    var enabled = toolbarItem.enabled;
+
+*/
