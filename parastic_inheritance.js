a30c256acfce8537c9d238a21db3cb00f6464d50
biroa <adam.biro@gmail.com>
2015-02-14 20:00:01 +0000

    Parastic inheritance

---
+++ parastic_inheritance.js
@@ -0,0 +1,35 @@
+var createPerson = function (firstName, lastName) {
+    var person = {
+        firstName: firstName,
+        lastName: lastName
+    };
+
+
+    Object.defineProperty(person, "fullName", {
+        get: function () {
+            return this.firstName + " " + this.lastName;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return person;
+};
+
+var createEmployee = function (firstName, lastName, position) {
+    var person = createPerson(firstName, lastName);
+
+    person.position = position;
+
+    var fullName = Object.getOwnPropertyDescriptor(person, "fullNAme");
+    var fullNameFunction = fullName.get.bind(person);
+
+    Object.defineProperty(person, "fullName", {
+        get: function () {
+            return fullNameFunction() + "," + this.position;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    return person;
+}
