0e3f3dea0ac32770f85909d6bbd2b826ba06058c
biroa <adam.biro@gmail.com>
2015-02-18 21:00:52 +0000

    add lines to event listeners1

--- eventtarget.js
+++ eventtarget.js
@@ -1,17 +1,27 @@
 var EventTarget = function(){
-
+    Object.definePropertiey(this,"__listeners",{
+        value: {}
+    });
 };
 
 Object.defineProperties(EventTarget.prototype,{
     addListener:{
         value: function(type, listener){
+            if(typeof this.__listeners[type] == undefined){
+                this.__listeners[type]= [];
+            }
 
+            this.__listeners[type].push(listener);
         },
         enumerable:true
     },
     removeListener: {
         value: function(type,listener){
+            var listeners = this.__listeners[type];
 
+            if(typeof listeners == undefined){
+                return;
+            }
         },
         enumerable:true
     }
