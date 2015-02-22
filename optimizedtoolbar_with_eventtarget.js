69c1a5724c5972e96be7c54137c6e2d522e5068a
biroa <adam.biro@gmail.com>
2015-02-22 20:53:40 +0000

    embed our eventListener

---
+++ optimizedtoolbar_with_eventtarget.js
@@ -0,0 +1,189 @@
+var tobject = (function (tobject) {
+
+    //Constructor function
+    var ToolbarItem = function (itemElement) {
+        EventTarget.call(this);
+        Object.defineProperty(this, "__el", {
+            value: itemElement
+        });
+    };
+
+    ToolbarItem.prototype = Object.create(EventTarget.prototype, {
+        toggleActiveState: {
+            value : function () {
+                this.activated = !this.activated;
+            },
+            enumerable : true
+        },
+        enabled: {
+            get: function () {
+                return !this.__el.classList.contains("disabled");
+            },
+            set: function (value) {
+                var currentValue = this.enabled;
+
+                if(currentValue == value){
+                    return;
+                }
+
+                if (value) {
+                    this.__el.classList.remove("disabled");
+                } else {
+                    this.__el.classList.add("disabled");
+                }
+
+                this.__fire({
+                    type:"enabledchange",
+                    value: value
+                });
+            }
+            //enabledChange
+        },
+        activated: {
+            get: function () {
+                return this.__el.classList.contains("active");
+            },
+            set: function (value) {
+                var currentValue = this.activated;
+
+                if(currentValue == value){
+                    return;
+                }
+
+                if (value) {
+                    this.__el.classList.add("active");
+                } else {
+                    this.__el.classList.remove("active");
+                }
+
+                this.__fire({
+                    type:"activatedchange",
+                    value: value
+                });
+
+            }
+        }
+        //activatedChange
+
+    });
+
+    var createToolbarItems = function (itemElements) {
+        var items = [];
+
+        [].forEach.call(itemElements, function (el, index, array) {
+            var item = new ToolbarItem(el);
+
+            items.push(item);
+        });
+
+        return items;
+    };
+
+    var Toolbar = function (toolbarElement) {
+        EventTarget.call(this);
+        var items = toolbarElement.querySelectorAll(".toolbar-item");
+
+        Object.defineProperties(this, {
+            __el: {
+                value: toolbarElement
+            },
+            items: {
+                value: createToolbarItems(items),
+                enumerable: true
+            }
+        });
+    };
+
+    Toolbar.prototype = Object.create(EventTarget.prototype, {
+        add: {
+            value: function (options) {
+                var span = document.createElement("SPAN");
+                span.className = "toolbar-item";
+
+                this.__el.appendChild(span);
+
+                var item = new ToolbarItem(span);
+
+                this.items.push(item);
+
+                this.__fire({
+                    type: "itemadded",
+                    item:item
+                });
+            },
+            enumerable : true
+        },
+        remove: {
+            value: function (index) {
+                var len = this.items.length;
+
+                if (index > len || index < 0) {
+                    throw new Error("Index is out of range");
+                }
+
+                var item = this.items[index];
+                this.items.splice(index, 1);
+
+                this.__el.removeChild(item.__el);
+
+                item = null;
+
+                this.__fire({
+                    type: "itemremoved"
+                });
+
+            },
+            enumerable : true
+        },
+        appendTo: {
+            value: function (parentElement) {
+                parentElement.appendChild(this.__el);
+
+                this.__fire({
+                    type: "appended",
+                    parentElement: parentElement
+                });
+
+            },
+            enumerable: true
+        }
+    });
+
+    tobject.createToolbar = function (elementId) {
+        var element = document.getElementById(elementId);
+
+        if (!element) {
+            element = document.createElement("DIV");
+            element.id = elementId;
+            element.className = "toolbar";
+        }
+
+        return new Toolbar(element);
+    };
+
+
+    return tobject;
+}(tobject || {}));
+
+//we add tobject to the func
+//if tobject not exist we add an empty obj
+
+
+/******************
+ Access of toolbar
+ ***************************************************
+ var toolbar = tobject.createToolbar("myToolbar");
+ toolbar.items[0].enabled = false;
+ toolbar.items[0].enabled = true;
+ toolbar.items[0].activated = true;
+ toolbar.items[0].activated = false;
+ toolbar.items[0].toggleActiveState();
+ ***************************************************
+ * Add a new toolbar and add remove elements from there
+ * *************************************************
+ *  var anotherToolbar = tobject.createToolbar("toolbarName");
+ *  anotherToolbar.appendTo(document.getElementById("myToolbar2"));
+ *  anotherToolbar.add();
+ *  anotherToolbar.remove(0);
+ *  anotherToolbar.items[0].enabled = false;
+ */
